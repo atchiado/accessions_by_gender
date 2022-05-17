2c31756c29277563e57b31de18878e86f5bc419b
Anthony Chiado <anthonychiado97@gmail.com>
2022-05-17 14:10:48 -0400

    Split data in to two data frames for plotting.

--- accessions_by_gender.R
+++ accessions_by_gender.R
@@ -3,8 +3,18 @@ library(ggtext)
 library(extrafont)
 
 
-# Load Data
+## Load Data
 accessions <- read.csv(file = "~/Desktop/work/data/r/accessions_by_gender/accessions_by_gender.csv")
 
 
-# Create Viz
+## Data Wrangling
+access <- accessions %>%
+            mutate(diff = Male - Female) %>%
+            pivot_longer(cols = c(Male, Female)) %>%
+            rename(Gender = name,
+                   Accessions = value)
+
+male_access <- access %>%
+                 filter(Gender == "Male")
+female_access <- access %>%
+                   filter(Gender == "Female")
