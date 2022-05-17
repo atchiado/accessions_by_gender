09c920515b3a22ede8a14607085674bfadd158cb
Anthony Chiado <anthonychiado97@gmail.com>
2022-05-17 13:58:50 -0400

    Initial Commit

---
+++ accessions_by_gender.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(ggtext)
+library(extrafont)
+
+
+# Load Data
+accessions <- read.csv(file = "~/Desktop/work/data/r/accessions_by_gender/accessions_by_gender.csv")
+
+
+# Create Viz
