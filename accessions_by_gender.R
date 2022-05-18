dbdf8d82ca845b3822f67a88eadfafc3be37f859
Anthony Chiado <anthonychiado97@gmail.com>
2022-05-18 13:02:40 -0400

    Enhanced viz.

--- accessions_by_gender.R
+++ accessions_by_gender.R
@@ -32,17 +32,59 @@ stats_female <- stats %>%
                   filter(Gender == "Female")
 
 diff <- access %>%
-          filter(Gender == "Male") %>%
+          filter(Gender == "Female") %>%
           mutate(x_pos = Accessions + (diff/2))
 
 
 # Data Viz
 ggplot(access) +
+  geom_rect(xmin = stats_male$meanneg, xmax = stats_male$meanpos,
+            ymin = 2021, ymax = 2006, fill = "#00bfc4", alpha = 0.1) +
+  geom_vline(xintercept = stats_male$mean, linetype = "solid",
+             size = 0.5, alpha = 0.5, color = "#00bfc4") +
+  geom_rect(xmin = stats_female$meanneg, xmax = stats_female$meanpos,
+            ymin = 2021, ymax = 2006, fill = "#f8766d", alpha = 0.1) +
+  geom_vline(xintercept = stats_female$mean, color = "#f8766d", linetype = "solid",
+             size = 0.5, alpha = 0.5) +
   geom_segment(data = male_access,
                aes(x = Accessions, y = Year,
                    yend = female_access$Year, xend = female_access$Accessions),
                color = "#aeb6bf",
                size = 4.5,
                alpha = 0.5) +
-  geom_point(aes(x = Accessions, y = Year, color = Gender), size = 4, show.legend = TRUE) +
-  ggtitle("Total Army 56A Accession Trends")
+  geom_point(aes(x = Accessions, y = Year, color = Gender), size = 4, show.legend = FALSE) +
+    scale_color_manual(values = c("#f8766d", "#00bfc4")) +
+  geom_text(data = diff,
+            aes(label = paste("D: ", diff), x = x_pos, y = Year),
+            fill = "white",
+            color = "#4a4e4d",
+            size = 2.5) +
+  geom_text(x = stats_male$mean - 1, y = 2006, label = "MEAN",
+            angle = 90, size = 2.5, color = "#00bfc4") +
+  geom_text(x = stats_male$meanpos - 1, y = 2006, label = "STDEV",
+            angle = 90, size = 2.5, color = "#00bfc4") +
+  facet_grid(Year ~ ., scales = "free", switch = "y") +
+  ggtitle("56A Accessions Trends") +
+  theme_minimal() +
+  theme(panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_line(color = "#4a4e4d"),
+        text = element_text(color = "#4a4e4d"),
+        strip.text.y.left = element_text(angle = 0),
+        panel.background = element_rect(fill = "white", color = "white"),
+        strip.background = element_rect(fill = "white", color = "white"),
+        strip.text = element_text(color = "#4a4e4d"),
+        plot.background = element_rect(fill = "white", color = "white"),
+        panel.spacing = unit(0, "lines"),
+        plot.margin = margin(1,1,0.5,1, "cm")) +
+  labs(subtitle = "<span style = 'color: #00bfc4;'>**Male**</span> and <span style = 'color: #f8766d;'>**Female**</span> Total Army Accessions from 2006 to 2021<br>",
+       caption = "Plot by Anthony Chiado") +
+  theme(plot.caption = element_markdown(hjust = 0, lineheight = 1.5),
+        plot.subtitle = element_markdown(size = 14, hjust = -.32),
+        plot.title = element_text(size = 16, hjust = -.06))
+
